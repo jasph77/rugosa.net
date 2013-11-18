d59d86aadc23cd22bed0e507f4efb107a319ceae
huangjian <diahuoji520@163.com>
2013-11-18 23:19:58 +0800

    2013年11月18日 23:19:56

---
+++ Index/Conf/config.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+	'URL_CASE_INSENSITIVE'   =>true,
+	'URL_HTML_SUFFIX'        =>'.shtml',
+	'url_model'              => '2', //URL模式
+);
+?>
---
+++ Index/Lib/Action/IndexAction.class.php
@@ -0,0 +1,9 @@
+<?php
+// 本类由系统自动生成，仅供测试用途
+class IndexAction extends Action {
+    public function index(){
+		$this->assign('title','ThinkPHP示例：模板继承');
+        $this->assign('var','这里是子模板定义内容!');
+        $this->display();
+    }
+}
