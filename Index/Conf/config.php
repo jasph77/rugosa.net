cdea97ae5d14c699c4b1ef0760f533c97c1f8fe7
huangjian <diahuoji520@163.com>
2013-11-23 23:41:32 +0800

    2013年11月23日 23:41:30

--- Index/Conf/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-return array(
-	//'配置项'=>'配置值'
-	'URL_CASE_INSENSITIVE'   =>true,
-	'URL_HTML_SUFFIX'        =>'.shtml',
-	'url_model'              => '2', //URL模式
-);
-?>
--- ROOT/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('APP_NAME','index');
-define('APP_DEBUG',true);
-define('APP_PATH','../Index/');
-define('THINK_PATH','../ThinkPHP/');
-define('URL_CASE_INSENSITIVE','true');
-require_once THINK_PATH.'ThinkPHP.php';
